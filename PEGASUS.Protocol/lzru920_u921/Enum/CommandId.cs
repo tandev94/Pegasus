fd76ca20907bd29a4c0fa59e4e2150563476ea60
tandev94 <56614953+tandev94@users.noreply.github.com>
2020-02-24 17:34:00 +0700

    aa.

--- PEGASUS.Protocol/lzru920_u921/Enum/CommandId.cs
+++ PEGASUS.Protocol/lzru920_u921/Enum/CommandId.cs
@@ -9,5 +9,14 @@ namespace PEGASUS.Protocol.lzru920_u921.Enum
     {
         public const ushort SETRAWDATAMODE = 50001;
         public const ushort SETRAWDATACONFIG = 50003;
+        public const ushort SETRAWDATACONFIGRESTORE = 50007;
+        public const ushort SETRAWDATAREDLASER = 50009;
+        public const ushort SETRAWDATAHEARTBEAT = 50012;
+        public const ushort GETRAWDATAINFORMATION = 50008;
+        public const ushort GETRAWDATAREDLASER = 50010;
+        public const ushort GETRAWDATAHEARTBEAT = 50013;
+        public const ushort SETRAWDATACONFIGSTORE = 50005;
+
+
     }
 }
