97434a121c69f0fca9c32a61e46c9e50a368dcf4
nuhupu <56197084+nuhupu@users.noreply.github.com>
2020-02-24 17:31:58 +0700

    edit

--- PEGASUS.Protocol/lzru920_u921/Enum/CommandId.cs
+++ PEGASUS.Protocol/lzru920_u921/Enum/CommandId.cs
@@ -7,7 +7,12 @@ namespace PEGASUS.Protocol.lzru920_u921.Enum
 {
     public class CommandId
     {
-        public const ushort SETRAWDATAMODE = 50001;
+        public const ushort SETRAWDATAMODE = 50001;//done
         public const ushort SETRAWDATACONFIG = 50003;
+        public const ushort GETRAWDATADISTANCEVALUES = 50011;
+        public const ushort SETRAWDATAERRORLOGRESET = 50006;//done
+        public const ushort SETRAWDATAFRAMECOUNTERRESET = 50014;//done
+        public const ushort GETRAWDATAMODE = 50002;//done
+        public const ushort GETRAWDATACONFIG = 50004;
     }
 }
